5dbc93dd54a97bce46a4fe14ab7595f8c1df05f1
Sam Andrello <samqandrello@gmail.com>
2024-06-20 16:11:11 -0400

    Initial commit

---
+++ README.rst
@@ -0,0 +1,21 @@
+============
+diffpy.utils
+============
+
+.. image:: https://github.com/sbillinge/diffpy.utils/actions/workflows/testing.yml/badge.svg
+   :target: https://github.com/sbillinge/diffpy.utils/actions/workflows/testing.yml
+
+
+.. image:: https://img.shields.io/pypi/v/diffpy.utils.svg
+        :target: https://pypi.python.org/pypi/diffpy.utils
+
+
+Python package for doing science.
+
+* Free software: 3-clause BSD license
+* Documentation: (COMING SOON!) https://sbillinge.github.io/diffpy.utils.
+
+Features
+--------
+
+* TODO
---
+++ src/diffpy/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge group
+#
+# See AUTHORS.rst for a list of people who contributed.
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""Blank namespace package for module diffpy."""
+
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+# End of file
+
